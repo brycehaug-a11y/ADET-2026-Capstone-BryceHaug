01a04d3f28ba0b2c82dc0bf0257aab220b9bb100
bhaug <bhaug08@gmail.com>
2026-05-07 11:00:05 -0500

    i gave up on this stupid shit

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-})
-export class AppModule {}
